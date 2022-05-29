1d7c7faacaf42beb605fbb1121db07d497a07f6d
Sunken Ahn <devsanso@outlook.com>
2022-05-29 19:53:33 +0900

    url:디렉토리 매핑설정을 json으로 분리, 완성 및 미 테스트

--- packages/middleware/page/index.ts
+++ packages/middleware/page/index.ts
@@ -1,33 +1,54 @@
 import {env} from 'process';
 import {join} from 'path';
-import express,{Request,Response,NextFunction} from 'express';
+import {existsSync} from 'fs';
 
+import {Request,Response,NextFunction} from 'express';
+import mappingJson from './mapping.json';
 
 const WWWPath = env.WWW_DIRECTORY != undefined ? env.WWW_DIRECTORY : "./www";
 
 
 const mapping : Map<string,string> = (()=>{
-    let m = new Map();
-    m.set("/","index");
-    m.set("/login","login");
-
+    let m = new Map(Object.entries(mappingJson));
     return m;
 })();
 
+const splitPath = (url : string) => url.split("/");
+
 const changePhyPath = (mapping : Map<string,string>,path : string) => mapping.get(path); 
 
-const combinePath = (base : string,path : string) => join(base,path);
+const MappingFilePath = (base : string,url : string,origin : string,directoyPath : string) => {
+    let p = url.replace(origin,directoyPath);
+    return join(base,p);
+}
 
+const matchString = (keys : Array<string>,str : string)  : string | null  => {
+    const find = keys.filter(value => {
+        value == str
+    });
+    if(find.length == 0) return null;
+
+    return find[0];
+}
 
 export default (req : Request,res : Response,next : NextFunction) =>{
     if(req.method = "GET") {
-        let directoryPath = changePhyPath(mapping,req.baseUrl);
-        if (directoryPath == undefined) {
-            next();
+        let first = splitPath(req.url)[1];
+        let directoryPath = matchString(Array.from(mapping.keys()),`/${first}`);
+
+        if(directoryPath != null) {
+            let filePath = MappingFilePath(WWWPath,req.url,`$/{first}`,directoryPath);
+            
+            if (existsSync(filePath)) {
+                res.sendFile(filePath);
+                res.status(200);
+            }else {
+                res.status(404);
+            }
             return;
         }
-        res.send(`hello world : ${directoryPath}`);
-        res.status(200);
+
+        next();
         return;
     }
 
