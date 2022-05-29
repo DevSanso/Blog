167eeec8e3e40f4fa349f759c5381f3e5af2535c
Sunken Ahn <devsanso@outlook.com>
2022-05-29 22:06:21 +0900

    작동 방식은 static처럼 변경,미들웨어 추가

--- packages/middleware/page/index.ts
+++ packages/middleware/page/index.ts
@@ -1,57 +1,38 @@
 import {env} from 'process';
-import {join} from 'path';
+import {join,parse} from 'path';
 import {existsSync} from 'fs';
 
 import {Request,Response,NextFunction} from 'express';
-import mappingJson from './mapping.json';
+import mappingJson from './mapping_ext.json';
 
 const WWWPath = env.WWW_DIRECTORY != undefined ? env.WWW_DIRECTORY : "./www";
-
-
-const mapping : Map<string,string> = (()=>{
-    let m = new Map(Object.entries(mappingJson));
-    return m;
+const mapping = (() => {
+    return Array.from(mappingJson);
 })();
 
-const splitPath = (url : string) => url.split("/");
 
-const changePhyPath = (mapping : Map<string,string>,path : string) => mapping.get(path); 
+const isMatchingExt = (url : string,mapping : Array<string>) => {
+    const ret = mapping.findIndex(function(value,index,array) {
+        return value == this.ext;
+    },parse(url));
+    return ret != -1? true : false;
+};
 
-const MappingFilePath = (base : string,url : string,origin : string,directoyPath : string) => {
-    let p = url.replace(origin,directoyPath);
-    return join(base,p);
-}
-
-const matchString = (keys : Array<string>,str : string)  : string | null  => {
-    const find = keys.filter(value => {
-        value == str
-    });
-    if(find.length == 0) return null;
-
-    return find[0];
-}
+const makeFilePathFromUrl = (root : string,url : string) => join(root,url);
 
 export default (req : Request,res : Response,next : NextFunction) =>{
-    if(req.method = "GET") {
-        let first = splitPath(req.url)[1];
-        let directoryPath = matchString(Array.from(mapping.keys()),`/${first}`);
 
-        if(directoryPath != null) {
-            let filePath = MappingFilePath(WWWPath,req.url,`$/{first}`,directoryPath);
-            
-            if (existsSync(filePath)) {
-                res.sendFile(filePath);
-                res.status(200);
-            }else {
-                res.status(404);
-            }
-            return;
+    if(req.method == "GET" && isMatchingExt(req.url,mapping)) {
+        const filePath = makeFilePathFromUrl(WWWPath,req.url);
+
+        if(existsSync(filePath))
+            res.status(400);
+        else {
+            res.sendFile(filePath);
+            res.status(200);
         }
-
-        next();
         return;
     }
 
     next();
-
 }
