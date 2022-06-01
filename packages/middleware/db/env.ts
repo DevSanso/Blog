e0c4bcf81d2e3f63897ec10ab1e17587bcb132f8
Sunken Ahn <devsanso@outlook.com>
2022-06-01 18:45:16 +0900

    오타 수정

--- packages/middleware/db/env.ts
+++ packages/middleware/db/env.ts
@@ -1,7 +1,9 @@
-const fn = (x : string | undefined,init : string) => x != undefined? x : init; 
+const fn = (x : string | undefined,init : string) => {
+    return x != undefined? x : init; 
+}
 
 export default {
-    host : fn(process.env.DATADATA_HOST,"localhost"),
+    host : fn(process.env.DATABASE_HOST,"localhost"),
     port : parseInt(fn(process.env.DATABASE_PORT,"3306")),
     user : fn(process.env.DATABASE_USER,"root"),
     password : fn(process.env.DATABASE_PASSWORD,""),
