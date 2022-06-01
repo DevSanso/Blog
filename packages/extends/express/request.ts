1bec8fa8fde516a4b2d84bf982b1bee3dbc40369
Sunken Ahn <devsanso@outlook.com>
2022-06-01 18:06:29 +0900

    express request  재정의

--- packages/extends/express/request.ts
+++ packages/extends/express/request.ts
@@ -1,9 +1,13 @@
 import {Request} from 'express';
+import {PoolConnection} from 'mysql2/promise';
 
-
+interface DBPool {
+    getDbConnection() : Promise<PoolConnection>            
+}
 
 declare module 'express' {
     interface Request {
-        
+        dbPool : DBPool
     }
 };
+
