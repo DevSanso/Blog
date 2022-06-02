94046fc30781da31f48a606705e4ee6b4371a566
Sunken Ahn <devsanso@outlook.com>
2022-06-02 23:42:33 +0900

    read 기능  삭제,editor 모듈 경로,해당 모듈 packages dep 키값 수정

--- packages/express_singleton/router.ts
+++ packages/express_singleton/router.ts
@@ -1,10 +1,10 @@
 import {Express} from 'express';
 import user from '@local/router_user';
-import editor from '@local/router_post';
+import editor from '@local/router_editor';
 
 
 
 export default  (e : Express) => {
     e.use("/user",user);
-    e.use("/post",editor);
+    e.use("/post/editor",editor);
 }
