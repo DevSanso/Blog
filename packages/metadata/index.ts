01d6a16804b232e4bef31ce605607ccf9c3b8b38
Sunken Ahn <devsanso@outlook.com>
2022-06-02 20:03:53 +0900

    인터페이스 추가

--- packages/metadata/index.ts
+++ packages/metadata/index.ts
@@ -1,7 +1,9 @@
-import type _Post from './post';
+import type * as p from './post';
 import type _Admin from './admin';
 
 
 
-export type Post = _Post;
+
 export type Admin = _Admin;
+export type Post = p.Post;
+export type PostTag = p.PostTag;
