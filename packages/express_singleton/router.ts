f012b97a1342fac2d9fa1761a2d2bd3e3be0a6c9
Sunken Ahn <devsanso@outlook.com>
2022-06-02 22:05:28 +0900

    게시글 기능 라우터 작성, uuid 라이브러리 추가

--- packages/express_singleton/router.ts
+++ packages/express_singleton/router.ts
@@ -1,9 +1,10 @@
 import {Express} from 'express';
 import user from '@local/router_user';
-
+import editor from '@local/router_post';
 
 
 
 export default  (e : Express) => {
     e.use("/user",user);
+    e.use("/post",editor);
 }
