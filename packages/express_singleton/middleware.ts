7cb106657be269775008289bdd5d69329eb16f8e
Sunken Ahn <devsanso@outlook.com>
2022-06-04 21:07:43 +0900

    error 미들웨어 등록 객체 변경

--- packages/express_singleton/middleware.ts
+++ packages/express_singleton/middleware.ts
@@ -3,7 +3,7 @@ import session from 'express-session';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
 
-import error from "@local/middleware_error";
+
 import page from "@local/middleware_page";
 import db from "@local/middleware_db";
 import access from '@local/middleware_access_restriction';
@@ -39,7 +39,6 @@ const frameworkMiddleware = (e : Express) => {
 export default (e : Express) => {
     frameworkMiddleware(e);
 
-    e.use(error);
     e.use(page);
     e.use(db);
     e.use(access);
--- packages/express_singleton/router.ts
+++ packages/express_singleton/router.ts
@@ -2,10 +2,10 @@ import {Express} from 'express';
 import user from '@local/router_user';
 import editor from '@local/router_editor';
 import post from '@local/router_post';
-
+import error from "@local/middleware_error";
 
 export default  (e : Express) => {
-    e.use("/user",user);
-    e.use("/post/editor",editor);
-    e.use("/post",post);
+    e.use("/user",user.use(error));
+    e.use("/post/editor",editor.use(error));
+    e.use("/post",post.use(error));
 }
