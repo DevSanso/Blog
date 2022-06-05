3d36f4d8909360b69b4dd6cecf1a5e32c092dd44
Sunken Ahn <devsanso@outlook.com>
2022-06-05 13:33:36 +0900

    url : "/"일때 "/index.html" 변경 전처리,WWW_DIRECTORY 환경변수는 절대경로만 가능,일부 논리적 오류 수정

--- packages/middleware/page/index.ts
+++ packages/middleware/page/index.ts
@@ -1,11 +1,19 @@
 import {env} from 'process';
+import path from 'path';
 import {join,parse,ParsedPath} from 'path';
 import {existsSync} from 'fs';
 
 import {Request,Response,NextFunction} from 'express';
 import mappingJson from './mapping_ext.json';
 
-const WWWPath = env.WWW_DIRECTORY != undefined ? env.WWW_DIRECTORY : "./www";
+const WWWPath = env.WWW_DIRECTORY != undefined ? (()=> {
+    if(!path.isAbsolute(env.WWW_DIRECTORY)) {
+        console.error("plz set WWW_DIRECTORY path absolute path");
+        process.exit(1);
+    }
+    return env.WWW_DIRECTORY;
+})() : path.join(__dirname,"./www");
+
 const mapping = (() => {
     return Array.from(mappingJson);
 })();
@@ -21,15 +29,16 @@ const isMatchingExt = (url : string,mapping : Array<string>) => {
 const makeFilePathFromUrl = (root : string,url : string) => join(root,url);
 
 export default (req : Request,res : Response,next : NextFunction) =>{
-
+    if(req.method == "GET" && req.url == "/") 
+        req.url = "/index.html";
+    
     if(req.method == "GET" && isMatchingExt(req.url,mapping)) {
         const filePath = makeFilePathFromUrl(WWWPath,req.url);
-
-        if(existsSync(filePath))
-            res.status(400);
+        if(!existsSync(filePath))
+            res.sendStatus(400);
         else {
-            res.sendFile(filePath);
             res.status(200);
+            res.sendFile(filePath);
         }
         return;
     }
