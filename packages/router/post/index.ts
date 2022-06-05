b1243273f1fd864d47d44896bf6283d2e22af24b
Sunken Ahn <36612967+DevSanso@users.noreply.github.com>
2022-06-05 22:58:03 +0900

    www page 프로젝트 초기화 ,etc (#7)
    * spa 프로젝트 초기화
    
    * json 허용
    
    * url : "/"일때 "/index.html" 변경 전처리,WWW_DIRECTORY 환경변수는 절대경로만 가능,일부 논리적 오류 수정
    
    * select 함수는 ops 없을시 where문 빼고 실행하게 수정
    
    * post라우터에 title로 검색 기능 코드 생성, fuse.js 모듈 참조

--- packages/router/post/index.ts
+++ packages/router/post/index.ts
@@ -1,10 +1,12 @@
 import {Router} from 'express';
 
 import read from './read';
+import search from './search';
 
 const router = Router();
 
 router.get("",read);
+router.get("/search",search);
 
 
 export default router;
