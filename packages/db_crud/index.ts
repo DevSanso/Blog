12076d67499adabaf1c5b9b357c59699c395264a
Sunken Ahn <devsanso@outlook.com>
2022-06-08 19:01:03 +0900

    post image 관련 테이블 crud 기능 생성, 해당 함수들의 첫 파라미터를 promise<PoolConnection>에서 PoolConnection도 되도록 수정,editor create 리퀘스트에서 img 파라미터 추가

--- packages/db_crud/index.ts
+++ packages/db_crud/index.ts
@@ -1,4 +1,4 @@
-import {PoolConnection,RowDataPacket} from 'mysql2/promise';
+import type {PoolConnection,RowDataPacket} from 'mysql2/promise';
 
 import {ErrorException} from '@local/middleware_error/err_type';
 
@@ -7,25 +7,26 @@ import readFn from './query/read';
 import deleteFn from './query/delete';
 
 import {Ops} from './query/utils/op';
-import {PostField,TagField} from './fields';
+import {PostField,TagField,PostImgField} from './fields';
 
 
 
-type InterfaceKeys<T> = Array<keyof T>; 
+
+export type InterfaceKeys<T> = Array<keyof T>; 
 type Value<T ,P extends keyof T> = T | Pick<T,P>;
 
 
 
 interface CreateModel<CF> {
-    create<PICK extends keyof CF>(conn : Promise<PoolConnection>,field : InterfaceKeys<CF>,val : Value<CF,PICK>) : Promise<void>
+    create<PICK extends keyof CF>(conn :  Promise<PoolConnection> | PoolConnection,field : InterfaceKeys<CF>,val : Value<CF,PICK>) : Promise<void>
 }
 
 interface ReadModel<T> {
-    read(conn : Promise<PoolConnection>,field : InterfaceKeys<T>,ops : Ops<T> | null) : Promise<RowDataPacket[][]>
+    read(conn : Promise<PoolConnection> | PoolConnection,field : InterfaceKeys<T>,ops : Ops<T> | null) : Promise<RowDataPacket[][]>
 }
 
 interface DeleteModel<T> {
-    delete(conn : Promise<PoolConnection>,ops : Ops<T>) : Promise<void>;
+    delete(conn : Promise<PoolConnection> | PoolConnection,ops : Ops<T>) : Promise<void>;
 }
 
 const sort = <F,PICK extends keyof F>(f : InterfaceKeys<F>,val : Value<F,PICK>) : Array<string> => f.map(value => val[value as string])
@@ -37,16 +38,13 @@ class Model<T> implements CreateModel<T>,ReadModel<T>,DeleteModel<T> {
         this.table = tableName;
     }
 
-    public async create<PICK extends keyof T>(conn : Promise<PoolConnection>,field : InterfaceKeys<T>,val : Value<T,PICK>) {
-        const c = await conn;
-        await c.beginTransaction();
+    public async create<PICK extends keyof T>(conn : Promise<PoolConnection> | PoolConnection,field : InterfaceKeys<T>,val : Value<T,PICK>) {
+        const c = conn instanceof Promise ? await conn : conn;
 
         const query = createFn(this.table,field as string[],sort(field,val));
         try {
             await c.execute(query);
-            c.commit();
         }catch(e) {
-            c.rollback();
             const exception  : ErrorException= {
                 code : 507,
                 message : e,
@@ -55,24 +53,21 @@ class Model<T> implements CreateModel<T>,ReadModel<T>,DeleteModel<T> {
             throw exception;
         }
     }
-    public async read(conn : Promise<PoolConnection>,field : InterfaceKeys<T>,ops :Ops<T> | null) : Promise<RowDataPacket[][]> {
-        const c = await conn;
+    public async read(conn : Promise<PoolConnection> | PoolConnection,field : InterfaceKeys<T>,ops :Ops<T> | null) : Promise<RowDataPacket[][]> {
+        const c = conn instanceof Promise ? await conn : conn;
         const query = readFn(this.table,field,ops);
 
         return await c.query(query) as RowDataPacket[][];
        
     }
 
-    public async delete(conn : Promise<PoolConnection>,ops : Ops<T>) {
-        const c = await conn;
-        await c.beginTransaction();
+    public async delete(conn : Promise<PoolConnection> | PoolConnection,ops : Ops<T>) {
+        const c = conn instanceof Promise ? await conn : conn;
 
         const query = deleteFn(this.table,ops);
         try {
             await c.execute(query);
-            c.commit();
         }catch(e) {
-            c.rollback();
             const exception  : ErrorException= {
                 code : 507,
                 message : e,
@@ -93,6 +88,12 @@ export default {
         create : () : CreateModel<TagField> => new Model<TagField>("tags"),
         read : () : ReadModel<TagField> => new Model<TagField>("tags"),
         delete : () : DeleteModel<TagField> => new Model<TagField>("tags")
+
+    },
+    postImg : {
+        create : () : CreateModel<PostImgField> => new Model<PostImgField>("post_img"),
+        read : () : ReadModel<PostImgField> => new Model<PostImgField>("post_img"),
+        delete : () : DeleteModel<PostImgField> => new Model<PostImgField>("post_img")
 
     }
 }
